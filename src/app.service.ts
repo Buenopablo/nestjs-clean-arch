e7b3609481e75ef3f62dab7d8da20049008e85cb
Pablo <pablobuenoctn18@gmail.com>
2026-01-24 12:42:59 -0300

    Refactoring env config module

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
+@Module({
+  imports: [EnvConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
