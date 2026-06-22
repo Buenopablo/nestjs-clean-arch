ad8296d4da25e819e12b5b130183fd70b8651bce
Pablo <pablobuenoctn18@gmail.com>
2026-06-22 11:29:39 -0300

    UserEntity -  getters methods unit tests

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -9,4 +9,20 @@ export class UserEntity {
   constructor(public readonly props: UserProps) {
     this.props.createAd = this.props.createAd ?? new Date();
   }
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  get email(): string {
+    return this.props.email;
+  }
+
+  get password(): string {
+    return this.props.password;
+  }
+
+  get createAd(): Date {
+    return this.props.createAd as Date;
+  }
 }
